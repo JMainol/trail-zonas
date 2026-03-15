53136e8cb8cf27136039b26a152cbd22af0d8822
JMainol <JMainol@users.noreply.github.com>
2026-03-15 13:24:46 +0000

    deploy: ad1a61aa5991cef4d7f61f82f9ff5764b275013e

--- chunk-HXTKNOS2.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-C6Q5SG76.js";var t=[{path:"",loadComponent:()=>import("./chunk-HOEHOHHO.js").then(o=>o.ContactoComponent)}];export{t as CONTACTO_ROUTES};
---
+++ chunk-JJUY6GLZ.js
@@ -0,0 +1,1 @@
+import"./chunk-C6Q5SG76.js";var t=[{path:"",loadComponent:()=>import("./chunk-RJK3DN6I.js").then(o=>o.ContactoComponent)}];export{t as CONTACTO_ROUTES};
