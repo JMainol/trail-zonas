68765b8c1db079e6b34a67f3aebc99a120d0fbfd
JMainol <JMainol@users.noreply.github.com>
2026-03-16 20:57:01 +0000

    deploy: 23fc52b62b6c81ecd0d6e2429b9fcf022c04cd96

--- chunk-GLNMIQRT.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-C6Q5SG76.js";var t=[{path:"historia",loadComponent:()=>import("./chunk-Q4TFFO6P.js").then(o=>o.HistoriaComponent)},{path:"",redirectTo:"historia",pathMatch:"full"}];export{t as EXPLORACION_ROUTES};
